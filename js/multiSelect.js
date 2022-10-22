c288dd2a3a5990dae8ad3ff7eb8626bb9cf0e38e
loinguyenqzz@gmail.com <73170897+loinguyenqzz@users.noreply.github.com>
2022-10-23 02:25:06 +0700

    [feat]: multi select checkbox

--- js/multiSelect.js
+++ js/multiSelect.js
@@ -1,73 +1,72 @@
 $(document).ready(() => {
-    $(".multi-select__icon--show").each(function() {
-        $(this).click(() => {
+
+    $(".multi-select").each(function() {
+        const iconShow = $(this).children(".multi-select__icon--show")
+        const tagWrap = $(this).children(".tag-wrap")
+        const multiSelectWrap = $(this).children(".multi-select__wrap")
+        const inputSelectAll = $(multiSelectWrap).children(".multi-select__header").children("input")
+        const inputOptions = $(multiSelectWrap).find(".multi-select__option-wrap .m-checkbox")
+
+        // handle show list options
+        iconShow.click(function() {
             $(this).siblings(".multi-select__wrap").toggleClass("hide")
         })
-    })
-
-    $(".multi-select__wrap .m-checkbox").each(function() {
-        $(this).click(function() {
-            const isCheck = $(this)[0].checked
-            const tagNode = $(this).siblings()[0]
-            const tagValue = $(tagNode).text()
-            const tagId = `tag-${this.id}`
-
-            if (this.id == "multi-select-0") {
-                if(isCheck) {
-                    $(".multi-select__option .m-checkbox").each(function() {
-                        const tagNode = $(this).siblings()[0]
-                        const tagValue = $(tagNode).text()
-                        const tagId = `tag-${this.id}`
-
-                        if (!$(this)[0].checked) {
-                            addTag(tagId, tagValue)
-                            handleClickRemoveTag($('#' + tagId))
-                            $(this)[0].checked = true
-                        }
-                    })
-                } else {
-                    $(".multi-select__option .m-checkbox").each(function() {
-                        const tagId = `tag-${this.id}`
-                        $('#' + tagId).remove()
-                        $(this)[0].checked = false
-                    })
-                }
+        
+        // handle select all 
+        inputSelectAll.click(function() {
+            
+            if (this.checked) {
+                inputOptions.each(function () {
+                    if(!this.checked) {
+                        this.checked = true
+                        addTag(tagWrap, this.value, $(this).siblings().text())
+                    }
+                })
             } else {
-                if(isCheck) {
-                    addTag(tagId, tagValue)
-                    handleClickRemoveTag($('#' + tagId))
-                } else {
-                    $("#multi-select-0")[0].checked = false
-                    $('#' + tagId).remove()
-                }
+                inputOptions.each(function () {
+                    this.checked = false
+                    removeTag(tagWrap, this.value)
+                })
             }
-
             
         })
+
+        // handle click option
+        inputOptions.each(function() {
+            $(this).click(function() {
+                if (this.checked) {
+                    addTag(tagWrap, this.value, $(this).siblings().text())
+                } else {
+                    removeTag(tagWrap, this.value)
+                    inputSelectAll[0].checked = false
+                }
+            })
+        })
+    })
+})
+
+const addTag = (tagWrap, value, content) => {
+
+    // add tag
+    $(tagWrap).append(`<li value=${value} class="tag-picked">${content}
+    <div class="tag-picked__btn--close"></div>
+    </li>`)
+
+    const iconX = $(tagWrap).children(`li[value=${value}]`).children()
+    //add event click iconX remove tag
+    iconX.click(function() {
+        // remove tag
+        $(this).parent().remove()
+        
+        // disable checkbox
+        const inputCheckbox = $(tagWrap).siblings(".multi-select__wrap")
+        .find(`input[value=${value}]`)
+
+        inputCheckbox[0].checked = false
     })
 
-    const addTag = (tagId, tagValue) => {
-        $(".tag-wrap")
-        .append(`<li id=${tagId} class="tag-picked">${tagValue}
-        <div class="tag-picked__btn--close"></div>
-        </li>`)
-    }
+}
 
-    const handleClickRemoveTag = (tagNode) => {
-        $(tagNode.children(".tag-picked__btn--close")).click(function() {
-                const checkboxId = $(tagNode)[0].id.slice(4)
-
-                console.log(checkboxId);
-
-                //set checkbox option
-                $('#' + checkboxId)[0].checked = false
-
-                // remove tag element
-                tagNode.remove()
-
-                // set checkbox all
-                $("#multi-select-0")[0].checked = false
-            })
-    }
-
-})
+const removeTag = (tagWrap, value) => {
+    $(tagWrap).children(`li[value=${value}]`).remove()
+}
